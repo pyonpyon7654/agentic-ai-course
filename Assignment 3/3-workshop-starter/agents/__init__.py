6d05b7209040ede2a55bbc64421c134c7ba0d7e8
pyonpyon7654 <mahjiaqi@hotmail.com>
2026-09-22 20:10:58 +0800

    Completed Assignment

---
+++ Assignment 3/3-workshop-starter/agents/__init__.py
@@ -0,0 +1,9 @@
+"""
+Agents module for Singapore Kopitiam project.
+"""
+
+from .orchestrator import orchestrator
+from .participant import participant
+from .summarizer import summarizer
+
+__all__ = ['orchestrator', 'participant', 'summarizer']
